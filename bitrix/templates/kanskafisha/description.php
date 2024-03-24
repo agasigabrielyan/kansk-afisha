0199be4fbb7f30a093055fe36dd1d9c0210088f7
agasi <agasi79@list.ru>
2024-03-24 17:00:39 +0300

    Закоммитить после правок в .gitignore

--- bitrix/templates/kanskafisha/description.php
+++
@@ -1,9 +0,0 @@
-<?
-if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
-	die();
-
-$arTemplate = Array(
-	'NAME' => 'kanskafisha',
-	'DESCRIPTION' => 'Шаблон для сайта рекламного агентства'
-);
-?>
