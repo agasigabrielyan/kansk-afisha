639d980e3263f132a140e08b50907f82db7f72ad
agasi <agasi79@list.ru>
2024-03-24 22:48:23 +0300

    Закоммитить текущее состояние

--- bitrix/templates/kanskafisha/components/bitrix/news/kanskafisha_banners/bitrix/news.list/.default/result_modifier.php
+++
@@ -1,5 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-foreach($arResult['ITEMS'] as $key=>$arItem){
-    $changed_picture = CFile::ResizeImageGet($arItem['DETAIL_PICTURE'],array("width"=>"300","height"=>"300"),BX_RESIZE_IMAGE_EXACT);
-    $arResult['ITEMS'][$key]['CHANGED_PICTURE'] = $changed_picture;
-}
